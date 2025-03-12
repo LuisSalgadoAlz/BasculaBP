32062a600beff6e11f948cc91a0faa98f703ac72
LuisSalgadoAlz <luissalgado877878@gmail.com>
2025-03-12 10:14:51 -0600

    ports bascula

--- testing/app.js
+++ testing/app.js
@@ -1,6 +1,34 @@
 const express = require("express");
-const app = express()
+const { ReadlineParser } = require('@serialport/parser-readline');
 
-app.listen(3000, ()=>{
-    console.log("Server on")
-})
+const app = express();
+
+const {SerialPort} = require('serialport')
+
+// Ver puertos seriales activos
+SerialPort.list()
+.then((data) => console.log(data))
+.catch(err => console.log(err));
+
+
+//lectura de puerto
+
+let currentData = 0;
+app.get('/peso', function (req, res) {
+    res.send({
+        peso: String(currentData),
+    });
+});
+
+const port = new SerialPort({path: 'COM1', baudRate: 1200}, function (err) {
+    if (err) {
+        return console.log('Error: ', err.message);
+    }
+});
+
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r' }))
+parser.on('data', data => currentData = data);
+
+app.listen(3000, () => {
+  console.log(`Server ejecutandose en el puerto 3000`);
+});
