ae7c2e5ad42b892eb2fd81ad66735bf6df0bb942
LuisSalgadoAlz <luissalgado877878@gmail.com>
2025-03-20 16:28:54 -0600

    refactor: otencion de datos asincronas mejorada

--- BASCULA/src/hooks/formDataBoletas.js
+++ BASCULA/src/hooks/formDataBoletas.js
@@ -1,6 +1,20 @@
-export const getDataFormBoletas = ({ arr, type, pesoIn, pesoOut }) => {
-    if (type==1 && pesoIn){
-        return {...arr, pesoIn}
-    }
-    return {...arr, pesoOut}
-}
+/**
+ * 
+ * @param {*} arr 
+ * @param {*} type 
+ * @param {*} pesoIn 
+ * @param {*} pesoOut 
+ * @returns 
+ * Esto se convertira en el procesamiento de datos mas limpios y tambien el hook que verifica los 
+ * campos esten llenos y correctamente ingresados
+ */
+
+export const getDataFormBoletas = (arr, type, pesoIn, pesoOut) => {
+  if (type == 1 && pesoIn) {
+    return {
+      ...arr,
+      "Peso entrada": pesoIn.peso[0].replaceAll("lb", ""),
+    };
+  }
+  return { ...arr, pesoOut };
+};
