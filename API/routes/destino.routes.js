a522effbd20dcf8d4ebd68fbce5c7b7ac6874630
LuisSalgadoAlz <luissalgado877878@gmail.com>
2025-03-21 11:00:19 -0600

    fix: correciones en el api, eliminaicion de tablas redundantes

--- API/routes/destino.routes.js
+++
@@ -1,7 +0,0 @@
-const destino = require('express').Router();
-const { getDestinoParaBoletas } = require('../controllers/destino.controller.js')
-const verificarToken = require('../middlewares/authJWT.js')
-
-destino.get("/boletas", verificarToken, getDestinoParaBoletas);
-
-module.exports = destino;  
--- API/routes/origen.routes.js
+++
@@ -1,7 +0,0 @@
-const origen = require('express').Router();
-const { getOrigenParaBoletas } = require('../controllers/origen.controller.js')
-const verificarToken = require('../middlewares/authJWT.js')
-
-origen.get("/boletas", verificarToken, getOrigenParaBoletas);
-
-module.exports = origen;  
