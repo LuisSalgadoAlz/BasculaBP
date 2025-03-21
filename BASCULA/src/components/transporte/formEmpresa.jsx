fb0c3b22fc9515280b691596df13151b2d0821d4
LuisSalgadoAlz <luissalgado877878@gmail.com>
2025-03-21 15:32:35 -0600

    feat: estilado de view de emrpesas y modal

---
+++ BASCULA/src/components/transporte/formEmpresa.jsx
@@ -0,0 +1,33 @@
+import { claseFormInputs } from "../../constants/boletas";
+const FormEmpresa = ({ fun }) => {
+  return (
+    <>
+      <div className="mt-5">
+        <label className="block mb-2 text-sm font-medium text-gray-900 ">
+          Nombre
+        </label>
+        <input type="text" name={"nombre"} className={claseFormInputs} placeholder={`Ingrese Nombre`} required onChange={fun} />
+      </div>
+      <div className="mt-5">
+        <label className="block mb-2 text-sm font-medium text-gray-900 ">
+          Email
+        </label>
+        <input type="email" name={"email"} className={claseFormInputs} placeholder={`Ingrese Email`} required onChange={fun} />
+      </div>
+      <div className="mt-5">
+        <label className="block mb-2 text-sm font-medium text-gray-900 ">
+          Número
+        </label>
+        <input type="number" name={"telefono"} className={claseFormInputs} placeholder={`Ingrese Número`} required onChange={fun} />
+      </div>
+      <div className="mt-5">
+        <label className="block mb-2 text-sm font-medium text-gray-900 ">
+          Descripción
+        </label>
+        <input type="text" name={"descripcion"} className={claseFormInputs} placeholder={`Ingrese Descripción`} required onChange={fun} />
+      </div>
+    </>
+  );
+};
+
+export default FormEmpresa;
--- BASCULA/src/components/transporte/search.jsx
+++ BASCULA/src/components/transporte/search.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
-import {ButtonAdd} from "../buttons";
+import { ButtonAdd } from "../buttons";
 import TableComponent from "../table";
+import FormEmpresa from "./formEmpresa";
 
 const Search = () => {
-    const [isOpen, setIsOpen] = useState()
+  const [isOpen, setIsOpen] = useState(false);
+  const [formData, setFormData] = useState();
 
-    const handleClick = () => {
-        setIsOpen(!isOpen)
-    }
+  const toggleModal = () => setIsOpen(!isOpen);
 
-    return (
+  const handleData = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+        ...prev,
+        [name] : value
+    }))
+  };
+
+  const handleSubmit = () => {
+    console.log(formData)
+    setIsOpen(false)
+  }
+  return (
     <>
       <div className="filtros grid grid-rows-1 grid-cols-12 grid-flow-col gap-1.5">
         <input
@@ -20,12 +32,35 @@ const Search = () => {
         <select className="py-2.5 px-4 text-sm font-medium text-gray-600  rounded-lg border border-gray-200">
           <option value="0">Tipo</option>
         </select>
-        <ButtonAdd name="Agregar" />
+        <ButtonAdd name="Agregar" fun={toggleModal} />
       </div>
       <div className="mt-7">
         <TableComponent />
       </div>
 
+      {isOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-opa-50">
+          <div className="bg-white p-8 rounded-2xl shadow-xl max-w-sm border border-gray-300">
+            <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+              Añadir nueva empresa
+            </h2>
+            <div>
+              <FormEmpresa fun={handleData} />
+            </div>
+            <div className="mt-6 flex justify-end space-x-4">
+              <button
+                onClick={toggleModal}
+                className="px-6 py-2 bg-gray-200 text-gray-700 rounded-lg transition duration-300 ease-in-out transform hover:bg-gray-300 hover:scale-105"
+              >
+                Cancelar
+              </button>
+              <button onClick={handleSubmit} className="px-6 py-2 text-white bg-green-600 rounded-lg hover:bg-green-700 transition duration-300 ease-in-out transform hover:scale-105">
+                Agregar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
