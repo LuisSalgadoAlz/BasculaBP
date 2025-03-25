fbd310450f2b164d894d0606e766f0b7d8cf881d
LuisSalgadoAlz <luissalgado877878@gmail.com>
2025-03-25 11:51:39 -0600

    eat: se anexo las rutas de socios, metodo get por id y update

--- API/controllers/socios.controller.js
+++ API/controllers/socios.controller.js
@@ -4,32 +4,54 @@ const db = new PrismaClient();
 
 const getSocios = async (req, res) => {
   const data = await db.socios.findMany();
-  const clenData = data.map((el)=>({
-    'id' : el.id,  
-    'Nombre' : el.nombre,
-    'Tipo' : el.tipo == 0 ? 'Proveedor' : 'Cliente', 
-    'Telefono' : el.telefono, 
-    'Correo' : el.correo,
-    'Estado' : el.estado
-  }))
+  const clenData = data.map((el) => ({
+    id: el.id,
+    Nombre: el.nombre,
+    Tipo: el.tipo == 0 ? "Proveedor" : "Cliente",
+    Telefono: el.telefono,
+    Correo: el.correo,
+    Estado: el.estado,
+  }));
   res.json(clenData);
 };
 
-const getStats = async (req, res) => {
-    try {
-        const [totalSocios, totalProveedores, totalClientes] = await Promise.all([
-            db.socios.count(),  // Cuenta el total de socios
-            db.socios.count({ where: { tipo: 0 } }),  // Cuenta los proveedores
-            db.socios.count({ where: { tipo: 1 } }),  // Cuenta los clientes
-        ]);
+const getSocioPorID = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-        res.json({ totalSocios, totalProveedores, totalClientes });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error al obtener las estadísticas de socios" });
+    const socio = await db.socios.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    if (!socio) {
+      return res.status(404).json({ message: "Socio no encontrado" });
     }
+
+    return res.status(200).json(socio);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Error en el servidor" });
+  }
 };
 
+const getStats = async (req, res) => {
+  try {
+    const [totalSocios, totalProveedores, totalClientes] = await Promise.all([
+      db.socios.count(), // Cuenta el total de socios
+      db.socios.count({ where: { tipo: 0 } }), // Cuenta los proveedores
+      db.socios.count({ where: { tipo: 1 } }), // Cuenta los clientes
+    ]);
+
+    res.json({ totalSocios, totalProveedores, totalClientes });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Error al obtener las estadísticas de socios" });
+  }
+};
 
 const postSocios = async (req, res) => {
   try {
@@ -53,4 +75,37 @@ const postSocios = async (req, res) => {
     res.status(500).json({ msg: `Error al crear usuario: ${error.message}` });
   }
 };
-module.exports = { getSocios, postSocios, getStats };
+
+const updateSocios = async (req, res) => {
+  const { nombre, tipo, telefono, correo, estado } = req.body;
+  try {
+    const upSocios = await db.socios.update({
+      where: {
+        id: parseInt(req.params.id),
+      },
+      data: {
+        nombre,
+        tipo: parseInt(tipo),
+        telefono,
+        correo,
+        estado:
+          estado == "true"
+            ? true
+            : estado === true || estado === true
+            ? estado
+            : false,
+      },
+    });
+    res.status(200).send("proceso exitoso");
+  } catch (error) {
+    res.status(401).send(`error ${error}`);
+  }
+};
+
+module.exports = {
+  getSocios,
+  postSocios,
+  getStats,
+  getSocioPorID,
+  updateSocios,
+};
--- API/routes/socios.routes.js
+++ API/routes/socios.routes.js
@@ -1,8 +1,10 @@
 const socios = require('express').Router();
-const { getSocios, postSocios, getStats } = require('../controllers/socios.controller.js')
+const { getSocios, postSocios, getStats, getSocioPorID, updateSocios } = require('../controllers/socios.controller.js')
 const verificarToken = require('../middlewares/authJWT.js')
 
 socios.get("/", verificarToken, getSocios);
 socios.post("/", verificarToken, postSocios);
 socios.get("/stats", verificarToken, getStats);
+socios.get("/:id", verificarToken, getSocioPorID);
+socios.put("/:id", verificarToken, updateSocios);
 module.exports = socios;  
