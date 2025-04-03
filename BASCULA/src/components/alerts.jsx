d1283baf5f63f818de2fb2525a5c949bba5e5e0a
LuisSalgadoAlz <luissalgado877878@gmail.com>
2025-04-03 13:41:54 -0600

    feat: modal de boletas recreado en vez de una pagina, se paso a modal, mas legible y con responsive

--- BASCULA/src/components/alerts.jsx
+++ BASCULA/src/components/alerts.jsx
@@ -1,4 +1,9 @@
-import { hoy } from "../constants/boletas";
+import { useState, useEffect } from "react";
+import { cargando, claseFormInputs, classFormSelct, hoy } from "../constants/boletas";
+import { ESTADOS_BOLETAS, URLHOST } from "../constants/global";
+import Cookies from 'js-cookie'
+import SelectFormBoletas from "./boletas/select";
+import { InputsFormBoletas } from "./boletas/inputs";
 
 export const ModalSuccess = ({ name, hdClose }) => {
   return (
@@ -116,55 +121,62 @@ export const NoData = () => {
   );
 };
 
-export const ModalBoletas = ({hdlClose}) => {
+export const ModalBoletas = ({hdlClose, handleChange}) => {
   return (
-    <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 bg-opa-50">
-      <div class="bg-white w-[1000px] rounded-2xl p-6 shadow-lg max-sm:w-[300px] max-sm:h-[500px] max-sm:overflow-auto">
-        <div class="mb-6">
-          <h2 class="text-2xl font-bold text-gray-800">Boletas</h2>
-          <p class="text-sm text-gray-500">Gestión de entrada y salida de material del {hoy.toLocaleDateString()}</p>
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 p-4 sm:p-6 min-h-screen bg-opa-50">
+    <div className="bg-white w-full max-w-4xl rounded-2xl p-6 shadow-lg sm:w-[90%] md:w-[80%] lg:w-[60%] max-h-[90vh] overflow-y-auto boletas">
+      <div className="mb-6">
+        <h2 className="text-2xl font-bold text-gray-800">Boletas</h2>
+        <p className="text-sm text-gray-500">Gestión de entrada y salida de material del {new Date().toLocaleDateString()}</p>
+      </div>
+
+      <div className="mt-6 p-4 bg-gray-100 rounded-lg border border-gray-300">
+        <h3 className="text-lg font-semibold text-gray-700">Lectura de Peso</h3>
+        <div className="flex justify-between items-center mt-2">
+          <span className="text-xl font-bold text-gray-900">Peso Actual:</span>
+          <span className="text-xl font-bold text-gray-900">0 Lb</span>
         </div>
-        <div class="grid grid-cols-2 gap-4">
-          Aqui va el form
+        <p className="text-sm text-gray-500">Peso en tiempo real de la báscula</p>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 p-2 mt-4 place-content-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+          {["Proceso", "Placa", "Clientes", "Transportes", "Motoristas", "Producto", "Origen", "Destino", "Flete"].map((item) => (
+            <SelectFormBoletas key={item} classCss={classFormSelct} name={item} data={cargando} fun={handleChange} />
+          ))}
         </div>
-        <div class="grid grid-cols-2 gap-4">
-          Aqui va el form
-        </div>
-        <div class="grid grid-cols-2 gap-4">
-          Aqui va el form
-        </div>
-        <div class="grid grid-cols-2 gap-4">
-          Aqui va el form
-        </div>
-        <div class="grid grid-cols-2 gap-4">
-          Aqui va el form
-        </div>
-        <div class="grid grid-cols-2 gap-4">
-          Aqui va el form
-        </div>
-        <div class="grid grid-cols-2 gap-4">
-          Aqui va el form
-        </div>
-        <div class="grid grid-cols-2 gap-4">
-          Aqui va el form
-        </div>
-        <div class="grid grid-cols-2 gap-4">
-          Aqui va el form
-        </div>
-        <div class="mt-6 p-4 bg-gray-100 rounded-lg border border-gray-300">
-          <h3 class="text-lg font-semibold text-gray-700">Lectura de Peso</h3>
-          <div class="flex justify-between items-center mt-2">
-            <span class="text-xl font-bold text-gray-900">Peso Actual:</span>
-            <span class="text-xl font-bold text-gray-900">0 kg</span>
-          </div>
-          <p class="text-sm text-gray-500">Peso en tiempo real de la báscula</p>
-        </div>
-        <div class="flex justify-end mt-6 space-x-4">
-          <button onClick={hdlClose} class="px-4 py-2 bg-gray-300 text-gray-700 rounded-lg hover:bg-gray-400 transition-transform duration-300 ease-in-out hover:scale-105">Cancelar</button>
-          <button class="px-4 py-2 text-white bg-green-600 rounded-lg hover:bg-green-700 transition-transform duration-300 ease-in-out hover:scale-105">Guardar</button>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-2  place-content-center">
+          {["Documento", "Peso entrada", "Peso salida", "Peso teorico", "Peso neto", "Desviacion", "Observacion"].map((item) => (
+            <InputsFormBoletas key={item} data={claseFormInputs} name={item} />
+          ))}
+          <label>Fecha de entrada</label>
+          <input type="datetime-local" name='fechaEntrada' className={claseFormInputs} disabled/>
+          <label>Fecha de salida</label>
+          <input type="datetime-local" name='fechaSalida' className={claseFormInputs} disabled/>
         </div>
       </div>
+
+      <div className="mt-4">
+        <SelectFormBoletas classCss={classFormSelct} name={'Estado'} data={ESTADOS_BOLETAS} fun={handleChange} />
+      </div>
+
+      <hr className="text-gray-300 mt-7" />
+      <div className="flex flex-col sm:flex-row justify-end mt-6 space-y-2 sm:space-y-0 sm:space-x-4">
+        <button
+          onClick={hdlClose}
+          className="px-4 py-2 bg-gray-300 text-gray-700 rounded-lg hover:bg-gray-400 transition-transform duration-300 ease-in-out hover:scale-105"
+        >
+          Cancelar
+        </button>
+        <button
+          className="px-4 py-2 text-white bg-green-600 rounded-lg hover:bg-green-700 transition-transform duration-300 ease-in-out hover:scale-105"
+        >
+          Guardar
+        </button>
+      </div>
     </div>
+  </div>
 
   );
 };
--- BASCULA/src/constants/global.js
+++ BASCULA/src/constants/global.js
@@ -1,1 +1,15 @@
 export const URLHOST = 'http://192.9.100.56:3000/'
+export const ESTADOS_BOLETAS = [
+    {
+        id : 0, 
+        nombre : '🟡 Pendiente'
+    },
+    {
+        id : 1, 
+        nombre : '🟢 Completado'
+    }, 
+    {
+        id : 2, 
+        nombre : '🔴 Peso fuera de tolerancia'
+    }, 
+]
