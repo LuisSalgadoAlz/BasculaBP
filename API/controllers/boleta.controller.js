4842349252ad9f511a325c6c5163ebc1d497e1c4
LuisSalgadoAlz <luissalgado877878@gmail.com>
2025-04-04 15:24:50 -0600

    feat: version preliminar del filtrado del modal de boletas, usando un objeto de arrays para obtener el filtrado cambiante

--- API/controllers/boleta.controller.js
+++ API/controllers/boleta.controller.js
@@ -7,65 +7,76 @@ const getAllData = async (req, res) => {
         /* 0 Proveedor 1 Cliente */
         const tipo = req.query.tipo !== undefined ? parseInt(req.query.tipo) : undefined;
         const socio = req.query.socio !== undefined ? parseInt(req.query.socio) : undefined;
-        const placa = req.query.placa || '';
+        const placa = req.query.placa !== undefined ? req.query.placa : undefined
         const empresa = req.query.empresa !== undefined ? parseInt(req.query.empresa) : undefined
+        const mts = req.query.motorista !== undefined ? parseInt(req.query.motorista) : undefined;
 
-        const dataAndFilter = await db.socios.findMany({
-            select: {
-              id: true, 
-              nombre: true, 
-              drSocios: {
-                select: {
-                  id: true, 
-                  nombre: true
-                }
-              },
-              rEmpresa: {
-                select: {
-                  id: true, 
-                  nombre: true,
-                  rMotoristaEmpresa: {
-                    select: {
-                      id: true, 
-                      nombre: true
-                    },
-                  }, 
-                  rVehiculoEmpresa: {
-                    select: {
-                      id: true, 
-                      placa: true
+        const [socios, empresas, vehiculos, motoristas] = await Promise.all([
+            db.socios.findMany({
+                select: {id: true, nombre: true},
+                where: {
+                    ...(tipo ? { tipo : tipo } : {}),
+                    rEmpresa: {
+                        some: {
+                            ...(empresa ? {id: empresa} : {}),
+                            rVehiculoEmpresa : {
+                                some : {...(placa ? {placa : placa} : {})}
+                            }, 
+                            rMotoristaEmpresa : {
+                                some : {...mts ? {id: mts} : {}}
+                            }
+                        },
                     }
-                  }
-                },
-                where : {
-                    id: empresa,
+                } 
+            }), 
+            db.empresa.findMany({
+                select: {id: true, nombre: true}, 
+                where: {
+                    rClientes: {
+                        is: {...(socio ? {id: socio} : {})}
+                    }, 
                     rVehiculoEmpresa : {
-                        some:{
-                            ...(placa ? {placa : placa } : {})
+                        some : {...(placa ? {placa : placa} : {})}
+                    }, 
+                    rMotoristaEmpresa : {
+                        some : {...mts ? {id: mts} : {}}
+                    }
+                }
+            }), 
+            db.vehiculo.findMany({
+                select : {id: true, placa : true}, 
+                where : {
+                    placa : placa, 
+                    rEmpresaVehiculo : {
+                        is : {
+                            ...(empresa ? {id:empresa} : {}), 
+                            rClientes : {
+                                is : {
+                                    ...(socio ? {id: socio} : {})
+                                }
+                            }
+                        }, 
+                    }
+                }
+            }), 
+            db.motoristas.findMany({
+                select : {id: true, nombre : true}, 
+                where : {
+                    id: mts,
+                    rEmpresaM : {
+                        is : {
+                            ...(empresa ? {id:empresa} : {}), 
+                            rClientes : {
+                                is : {
+                                    ...(socio ? {id: socio} : {})
+                                }
+                            }
                         }
                     }
                 }
-              },
-            },
-            where: { 
-              estado: true, 
-              ...(tipo !== '' ? {tipo: {equals: tipo}} : {}),
-              ...(socio ? {id: socio} : {}),
-              rEmpresa: {
-                some: {
-                  ...(placa ? {
-                    rVehiculoEmpresa: {
-                      some: {
-                        placa: placa
-                      }
-                    }
-                  } : {})
-                }
-              }
-            }
-          });
-
-        res.status(200).json(dataAndFilter);
+            })
+        ]);
+        res.status(200).json({socios, empresas, vehiculos, motoristas});
     }catch (err) {
         console.log(err)
     }
