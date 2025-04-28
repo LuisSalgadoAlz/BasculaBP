dc2be580aa2c3fb8727b15b1a1ab77308a4bf97b
LuisSalgadoAlz <luissalgado877878@gmail.com>
2025-04-28 16:25:52 -0600

    fix: se arreglo que permita campos nullos duplicados en la base de datos, pero manteniendo la validacion de claves duplicadoaas

---
+++ API/prisma/migrations/20250428221727_init/migration.sql
@@ -0,0 +1,19 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- DropIndex
+ALTER TABLE [dbo].[Motoristas] DROP CONSTRAINT [Motoristas_correo_key];
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
