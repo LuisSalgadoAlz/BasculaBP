fb48f2257073e0dab5574e06bf065d5b0e0728e4
LuisSalgadoAlz <luissalgado877878@gmail.com>
2025-04-29 15:09:03 -0600

    feat: migraciones del servidor

--- API/prisma/migrations/20250429193818_init/migration.sql
+++ API/prisma/migrations/20250429193818_init/migration.sql
@@ -5,38 +5,39 @@ BEGIN TRAN;
 -- CreateTable
 CREATE TABLE [dbo].[Boleta] (
     [id] INT NOT NULL IDENTITY(1,1),
-    [boletaType] INT NOT NULL,
-    [idPlaca] INT NOT NULL,
+    [boletaType] INT,
+    [idPlaca] INT,
     [placa] NVARCHAR(1000) NOT NULL,
-    [idEmpresa] INT NOT NULL,
+    [idEmpresa] INT,
     [empresa] NVARCHAR(1000) NOT NULL,
-    [idSocio] INT NOT NULL,
+    [idSocio] INT,
     [socio] NVARCHAR(1000) NOT NULL,
-    [idProducto] INT NOT NULL,
-    [producto] NVARCHAR(1000) NOT NULL,
-    [proceso] INT NOT NULL,
+    [idProducto] INT,
+    [producto] NVARCHAR(1000),
+    [proceso] INT,
     [idOrigen] INT,
-    [origen] NVARCHAR(1000) NOT NULL,
+    [origen] NVARCHAR(1000),
     [idDestino] INT,
     [destino] NVARCHAR(1000),
-    [idTransladoOrigen] INT,
-    [transladoOrigen] NVARCHAR(1000),
-    [idTransladoDestino] NVARCHAR(1000),
-    [transladoDestino] INT,
-    [idMovimiento] INT NOT NULL,
+    [idTrasladoOrigen] INT,
+    [trasladoOrigen] NVARCHAR(1000),
+    [idTrasladoDestino] INT,
+    [trasladoDestino] NVARCHAR(1000),
+    [idMovimiento] INT,
     [movimiento] NVARCHAR(1000),
     [manifiesto] INT,
     [estado] NVARCHAR(1000) NOT NULL,
-    [impreso] BIT,
+    [impreso] DATETIME2,
     [idUsuario] INT NOT NULL,
     [usuario] NVARCHAR(1000) NOT NULL,
-    [idMotorista] INT NOT NULL,
+    [idMotorista] INT,
     [motorista] NVARCHAR(1000) NOT NULL,
     [ordenDeCompra] INT,
+    [ordenDeTransferencia] INT,
     [pesoTeorico] FLOAT(53),
     [pesoNeto] FLOAT(53),
     [desviacion] FLOAT(53),
-    [observaciones] VARCHAR(255) NOT NULL,
+    [observaciones] VARCHAR(255),
     [pesoInicial] FLOAT(53) NOT NULL,
     [pesoFinal] FLOAT(53),
     [fechaInicio] DATETIME2 NOT NULL,
@@ -88,8 +89,7 @@ CREATE TABLE [dbo].[Motoristas] (
     [correo] NVARCHAR(1000),
     [estado] BIT NOT NULL,
     [idEmpresa] INT NOT NULL,
-    CONSTRAINT [Motoristas_pkey] PRIMARY KEY CLUSTERED ([id]),
-    CONSTRAINT [Motoristas_correo_key] UNIQUE NONCLUSTERED ([correo])
+    CONSTRAINT [Motoristas_pkey] PRIMARY KEY CLUSTERED ([id])
 );
 
 -- CreateTable
@@ -124,6 +124,7 @@ CREATE TABLE [dbo].[Socios] (
 -- CreateTable
 CREATE TABLE [dbo].[Translado] (
     [id] INT NOT NULL IDENTITY(1,1),
+    [code] NVARCHAR(1000) NOT NULL,
     [nombre] NVARCHAR(1000) NOT NULL,
     [tipo] INT NOT NULL,
     [estado] BIT NOT NULL,
@@ -165,16 +166,16 @@ ALTER TABLE [dbo].[Boleta] ADD CONSTRAINT [Boleta_idUsuario_fkey] FOREIGN KEY ([
 ALTER TABLE [dbo].[Boleta] ADD CONSTRAINT [Boleta_idEmpresa_fkey] FOREIGN KEY ([idEmpresa]) REFERENCES [dbo].[Empresa]([id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE [dbo].[Boleta] ADD CONSTRAINT [Boleta_idSocio_fkey] FOREIGN KEY ([idSocio]) REFERENCES [dbo].[Socios]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE [dbo].[Boleta] ADD CONSTRAINT [Boleta_idSocio_fkey] FOREIGN KEY ([idSocio]) REFERENCES [dbo].[Socios]([id]) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE [dbo].[Boleta] ADD CONSTRAINT [Boleta_idProducto_fkey] FOREIGN KEY ([idProducto]) REFERENCES [dbo].[Producto]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE [dbo].[Boleta] ADD CONSTRAINT [Boleta_idProducto_fkey] FOREIGN KEY ([idProducto]) REFERENCES [dbo].[Producto]([id]) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE [dbo].[Boleta] ADD CONSTRAINT [Boleta_idMotorista_fkey] FOREIGN KEY ([idMotorista]) REFERENCES [dbo].[Motoristas]([id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE [dbo].[Boleta] ADD CONSTRAINT [Boleta_idMovimiento_fkey] FOREIGN KEY ([idMovimiento]) REFERENCES [dbo].[Movimientos]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
+ALTER TABLE [dbo].[Boleta] ADD CONSTRAINT [Boleta_idMovimiento_fkey] FOREIGN KEY ([idMovimiento]) REFERENCES [dbo].[Movimientos]([id]) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE [dbo].[Direcciones] ADD CONSTRAINT [Direcciones_idCliente_fkey] FOREIGN KEY ([idCliente]) REFERENCES [dbo].[Socios]([id]) ON DELETE NO ACTION ON UPDATE CASCADE;
