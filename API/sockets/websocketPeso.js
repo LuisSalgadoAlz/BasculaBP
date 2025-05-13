77a1484142fe8d924e407947a44bddc924bd3d4e
LuisSalgadoAlz <luissalgado877878@gmail.com>
2025-05-13 10:47:28 -0600

    feat: se agrego el logger a la conexion de la bascula.

--- API/sockets/websocketPeso.js
+++ API/sockets/websocketPeso.js
@@ -1,21 +1,26 @@
 // websocket.js
 const WebSocket = require("ws");
 const obtenerPeso = require("../controllers/basculaLive.controller");
+const { setLoggerSystema } = require("../utils/logger");
 
 const setupWebSocket = (server) => {
   const wss = new WebSocket.Server({ server });
-
   wss.on("connection", (ws) => {
+    let errorRegister=0;
     const intervalId = setInterval(async () => {
       try {
         const peso = await obtenerPeso();
         wss.clients.forEach((client) => {
           if (client.readyState === WebSocket.OPEN) {
-            client.send(`${peso}`);
+            client.send(`${pesso}`);
           }
         });
+        errorRegister = 0;
       } catch (err) {
-        console.error("Error al obtener el peso:", err.message);
+        if (errorRegister===0){
+          setLoggerSystema('SOCKET', 'ERROR OBTENCION DE PESO UNDEFINED', 3)
+          errorRegister++;
+        }
       }
     }, 100);
   
