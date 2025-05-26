199d172f323c5c415742dba13eb3326e04cf2208
LuisSalgadoAlz <luissalgado877878@gmail.com>
2025-05-26 08:51:49 -0600

    feat: se creo el endpoint para poder enviar mensajes a soporte, se mejoro tambien el manejo de imagenes con base 64.

--- API/controllers/soporte.controller.js
+++ API/controllers/soporte.controller.js
@@ -1,12 +1,26 @@
 const db = require('../lib/prisma')
 const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
 const {setLogger} = require('../utils/logger');
 const { alertaSoporte } = require('../utils/cuerposCorreo');
+const enviarCorreo = require('../utils/enviarCorreo');
 
-const enviarMailDeSoporte = (req, res) => {
+const enviarMailDeSoporte = async(req, res) => {
    try {
-    res.send({token: req.header('Authorization')})
+    const token = req.header('Authorization')
+    const verificado = jwt.verify(token, process.env.SECRET_KEY);
+    const usuario = await db.usuarios.findUnique({
+      where: {
+        usuarios: verificado["usuarios"],
+      },
+    });
+    const response = alertaSoporte(req.body, usuario, enviarCorreo)
+    if (response.exito){
+      return res.status(201).send({ msg: response.mensaje })
+    }
+    return res.status(201).send({ msgErr: response.mensaje })
    }catch(err) {
+    
     console.log(err)
    }
 }
