a2f81b1daf27cf1f26a9671823affcde6fb22d7b
Luis Salgado <luissalgado877878@gmail.com>
2025-06-03 16:20:28 -0600

    feat: modelo tolva

--- API/routes/tolva.routes.js
+++ API/routes/tolva.routes.js
@@ -1,8 +1,9 @@
 const tolva = require('express').Router();
-const { analizadorQR } = require('../controllers/tolva.controller.js');
+const { analizadorQR, getDataForSelectSilos } = require('../controllers/tolva.controller.js');
 const verificarToken = require('../middlewares/authJWT.js')
 const upload = require('../middlewares/upload.js');
 
+tolva.get("/silos", getDataForSelectSilos)
 tolva.post("/analizar-qr", upload.single('image'), analizadorQR);
 
 module.exports = tolva;  
