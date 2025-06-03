a2ce46573135fc20e9e1eefe701b2df852b1b0ba
Luis Salgado <luissalgado877878@gmail.com>
2025-06-03 16:20:04 -0600

    feat: se agrego el model de silos mas sus 31 diferentes silos.

---
+++ API/prisma/migrations/20250603213555_add_model_silos/migration.sql
@@ -0,0 +1,25 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- CreateTable
+CREATE TABLE [dbo].[Silos] (
+    [id] INT NOT NULL IDENTITY(1,1),
+    [nombre] NVARCHAR(1000) NOT NULL,
+    [capacidad] FLOAT(53),
+    [estado] BIT,
+    CONSTRAINT [Silos_pkey] PRIMARY KEY CLUSTERED ([id])
+);
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
