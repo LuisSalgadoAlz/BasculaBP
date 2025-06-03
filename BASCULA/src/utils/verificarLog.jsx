b9c08698568c0a2b75b5fce52dc0cdc036a40826
Luis Salgado <luissalgado877878@gmail.com>
2025-06-03 08:13:03 -0600

    refactor: se mejoro la legibilida del componente de verificacion del rutado. Haciendo mas escalable modificar y anexar mas rutas o vistas.

--- BASCULA/src/utils/verificarLog.jsx
+++ BASCULA/src/utils/verificarLog.jsx
@@ -1,14 +1,16 @@
 import { Navigate, Outlet, useNavigate } from "react-router";
-import {Cuerpo, CuerpoAdmin} from "../components/cuerpo";
+import {Cuerpo, CuerpoAdmin, CuerpoTolva} from "../components/cuerpo";
 import Cookies from 'js-cookie';
 import { useEffect, useMemo } from "react";
 import { ModalErr } from '../components/alerts'
+import { TOKEN_EXPIRY_MINUTES } from "../constants/global";
 
 const VerificarLog = ({Children, redirectTo='/', userType}) => {
     const navigate = useNavigate()
 
     const cuerpoMemo = useMemo(() => <Cuerpo><Outlet /></Cuerpo>, []);
     const cuerpoAdmin = useMemo(()=> <CuerpoAdmin><Outlet /></CuerpoAdmin>, [])
+    const cuerpoTolva = useMemo(()=> <CuerpoTolva><Outlet /></CuerpoTolva>, [])
 
     const sessionActive = () => {
         if (Cookies.get('token')) {
@@ -16,7 +18,7 @@ const VerificarLog = ({Children, redirectTo='/', userType}) => {
             /**
              * ? Tiempo de expiracion de 30 minutos 
              */
-            expirationDate.setMinutes(expirationDate.getMinutes() + 30);
+            expirationDate.setMinutes(expirationDate.getMinutes() + TOKEN_EXPIRY_MINUTES);
             Cookies.set('token', Cookies.get('token'), { expires: expirationDate });
             Cookies.set('type', Cookies.get('type'), { expires: expirationDate });
             Cookies.set('name', Cookies.get('name'), { expires: expirationDate });
@@ -40,6 +42,7 @@ const VerificarLog = ({Children, redirectTo='/', userType}) => {
 
     if (Cookies.get('type') == 'ADMINISTRADOR') return cuerpoAdmin;
     if (Cookies.get('type') == 'BASCULA') return cuerpoMemo;
+    if (Cookies.get('type') == 'TOLVA') return cuerpoTolva;
 }
  
 export default VerificarLog;
