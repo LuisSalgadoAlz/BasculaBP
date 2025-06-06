1f6805d4296bda6347b2d004ef9784bdb6bbe893
Luis Salgado <luissalgado877878@gmail.com>
2025-06-06 15:27:07 -0600

    feat: se creo el endpoint /silosfortime y /stats del departamento de tolva.

--- API/routes/tolva.routes.js
+++ API/routes/tolva.routes.js
@@ -1,9 +1,11 @@
 const tolva = require('express').Router();
-const { analizadorQR, getDataForSelectSilos, updateSiloInBoletas } = require('../controllers/tolva.controller.js');
+const { analizadorQR, getDataForSelectSilos, updateSiloInBoletas, getAsignForDay, getStatsForTolva } = require('../controllers/tolva.controller.js');
 const verificarToken = require('../middlewares/authJWT.js')
 const upload = require('../middlewares/upload.js');
 
 tolva.get("/silos", getDataForSelectSilos)
+tolva.get("/silosForTime", getAsignForDay)
+tolva.get("/stats", getStatsForTolva)
 tolva.post("/analizar-qr", upload.single('image'), analizadorQR);
 tolva.put("/add/silo/:id", updateSiloInBoletas)
 
