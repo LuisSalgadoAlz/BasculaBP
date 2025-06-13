9d4aa07635836097df64c006435133da9169492a
Luis Salgado <luissalgado877878@gmail.com>
2025-06-13 09:34:51 -0600

    feat: se agrego el rutado para acceder al controlador.

--- API/routes/tolva.routes.js
+++ API/routes/tolva.routes.js
@@ -1,11 +1,12 @@
 const tolva = require('express').Router();
-const { analizadorQR, getDataForSelectSilos, updateSiloInBoletas, getAsignForDay, getStatsForTolva } = require('../controllers/tolva.controller.js');
+const { analizadorQR, getDataForSelectSilos, updateSiloInBoletas, getAsignForDay, getStatsForTolva, buscarBoleSinQR } = require('../controllers/tolva.controller.js');
 const verificarToken = require('../middlewares/authJWT.js')
 const upload = require('../middlewares/upload.js');
 
 tolva.get("/silos", getDataForSelectSilos)
 tolva.get("/silosForTime", getAsignForDay)
 tolva.get("/stats", getStatsForTolva)
+tolva.get("/analizar-sin-qr", buscarBoleSinQR)
 tolva.post("/analizar-qr", upload.single('image'), analizadorQR);
 tolva.put("/add/silo/:id", updateSiloInBoletas)
 
