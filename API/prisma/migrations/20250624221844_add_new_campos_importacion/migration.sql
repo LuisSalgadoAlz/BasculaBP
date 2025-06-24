24c8dcb6159f70b2420aada58278f6d9af429470
Luis Salgado <luissalgado877878@gmail.com>
2025-06-24 16:19:36 -0600

    feat: nuevos campos en la boleta.

---
+++ API/prisma/migrations/20250624221844_add_new_campos_importacion/migration.sql
@@ -0,0 +1,20 @@
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[Boleta] ADD [bodegaPuerto] INT,
+[fechaDespachoPuerto] DATETIME2;
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
