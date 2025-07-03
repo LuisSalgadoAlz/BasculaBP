fbdf7cc421f77193ec1330c7211fd90a183b092b
Luis Salgado <luissalgado877878@gmail.com>
2025-07-03 16:21:14 -0600

    feat: cambio de nombre del controlador de guardia en la ruta.

--- API/routes/guardia.routes.js
+++ API/routes/guardia.routes.js
@@ -1,5 +1,5 @@
 const guardia = require('express').Router();
-const { getBuscarPlaca } = require('../controllers/guardia.js');
+const { getBuscarPlaca } = require('../controllers/guardia.controller.js');
 const verificarToken = require('../middlewares/authJWT.js')
 
 guardia.get("/buscarPlaca", getBuscarPlaca)
