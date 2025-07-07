e5d81894647f53884d066f444c95a5a952136085
Luis Salgado <luissalgado877878@gmail.com>
2025-07-07 08:37:11 -0600

    feat: mejoramiento de rendimiento de Sql server

--- API/ecosystem.config.js
+++ API/ecosystem.config.js
@@ -2,18 +2,36 @@
 module.exports = {
   apps: [
     {
-      name: "tolva",
+      name: "tolva-app",
       script: "app.js",
+      instances: "max",
+      exec_mode: "cluster",
+      max_memory_restart: "500M",
+      restart_delay: 5000,
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+      error_file: "./logs/tolva-error.log",
+      out_file: "./logs/tolva-out.log",
+      merge_logs: true,
       env: {
+        NODE_ENV: "production",
         PORT: 80,
         DIST_PATH: "../bascula/disttolva"
       }
     },
     {
-      name: "bascula",
+      name: "bascula-app",
       script: "app.js",
+      instances: "max",
+      exec_mode: "cluster",
+      max_memory_restart: "500M",
+      restart_delay: 5000,
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+      error_file: "./logs/bascula-error.log",
+      out_file: "./logs/bascula-out.log",
+      merge_logs: true,
       env: {
-        PORT: 3000, 
+        NODE_ENV: "production",
+        PORT: 3000,
         DIST_PATH: "../bascula/dist"
       }
     }
